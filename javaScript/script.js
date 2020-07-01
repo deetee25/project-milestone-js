bd763139204b32615a3cbc4efb972162dda1a41a
David Thompson <deetee81@gmail.com>
2020-07-01 00:59:57 +0000

    added arrow functions to retrive data from the fetch api and setup a fucntion countries

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,9 @@
 fetch("https://restcountries.eu/rest/v2/all")
+.then(res => res.json())
+.then(data => initialize(data))
+.catch(err => console.log("Error:", err));
+
+function initialize(countriesData) {
+  countries = countriesData;
+let options = "";
+}
